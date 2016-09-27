a4adf31fffc48dbbe8784cb3c3f86bc72ab9f7a1
DaftSpirit <joris.rat@gmail.com>
2016-09-27 10:28:50 +0200

    Update accueil_connected.php

--- accueil_connected.php
+++ accueil_connected.php
@@ -12,6 +12,7 @@
 						<textarea name="message" rows="10" cols="70"></textarea></br>
 						<input type="submit" value="Envoyer"/></br>
 						</fieldset>
+		    </form>
                     <script type="text/javascript" src="js/AWEoui.js"></script>
                 </article>
                 <aside>
